d4a5e00d8b3925e7c6a11e4e36b58e46b0ff9015
johnc <johncaboclo@gmail.com>
2024-08-05 23:08:55 -0300

    classes de teste uitário

--- src/main/java/com/example/demospringbootredismongodb/DemoSpringBootRedisComMongodbApplication.java
+++ src/main/java/com/example/demospringbootredismongodb/DemoSpringBootRedisComMongodbApplication.java
@@ -1,6 +1,5 @@
 package com.example.demospringbootredismongodb;
 
-import com.example.demospringbootredismongodb.entity.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -13,11 +12,6 @@ public class DemoSpringBootRedisComMongodbApplication {
 
 		SpringApplication.run(DemoSpringBootRedisComMongodbApplication.class, args);
 
-		Person person = Person.builder()
-				.id("123")
-				.name("John Doe")
-				.age(30)
-				.build();
 	}
 
 }
--- src/main/java/com/example/demospringbootredismongodb/service/PersonService.java
+++ src/main/java/com/example/demospringbootredismongodb/service/PersonService.java
@@ -4,7 +4,6 @@ import com.example.demospringbootredismongodb.entity.Person;
 import com.example.demospringbootredismongodb.repository.PersonRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
